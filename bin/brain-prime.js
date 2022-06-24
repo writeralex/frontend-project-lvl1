955fb538713adc60cba0d5c01b472c0f6e701fd9
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-06-24 22:22:55 +0300

    step 9: brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import startBrainPrimeGame from '../games/prime.js';
+startBrainPrimeGame();
---
+++ games/prime.js
@@ -0,0 +1,25 @@
+import { randomNumberGenerator } from '../src/random.js';
+import { gameEngine } from '../src/index.js';
+
+const startBrainPrimeGame = () => {
+    gameEngine(description, brainPrime);
+}
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no". ';
+
+export const brainPrime = () => {
+    const randomNumber = randomNumberGenerator(0, 10);
+    const rightAnswer = (isPrime(randomNumber) ? 'yes' : 'no');
+    return [randomNumber, rightAnswer];
+}
+
+const isPrime = (randomNumber) => {
+    for (let i = 2; i < randomNumber; i += 1) {
+        if (randomNumber % i === 0) {
+            return false;
+        };
+        return true;
+    };
+}
+
+export default startBrainPrimeGame;
