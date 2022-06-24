9b6e5be8d78223698d676451aff1dc6ed68268d0
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-06-24 18:30:43 +0300

    step 7: brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import startBrainGcdGame from '../games/gcd.js';
+startBrainGcdGame();
---
+++ games/gcd.js
@@ -0,0 +1,46 @@
+import { randomNumberGenerator } from '../src/random.js';
+import { gameEngine } from '../src/index.js';
+
+const startBrainGcdGame = () => {
+    gameEngine(description, brainGcd);
+}
+
+const description = 'Find the greatest common divisor of given numbers. ';
+
+export const brainGcd = () => {
+    const randomNumber1 = randomNumberGenerator(0, 10);
+    const randomNumber2 = randomNumberGenerator(0, 10);
+    const randomTwoNumbers = `${randomNumber1} ${randomNumber2}`;
+
+    let arrayOfNumber1 = [];
+    let arrayOfNumber2 = [];
+
+    for (let i = 1; i <= randomNumber1; i += 1) {
+        if (randomNumber1 % i === 0) {
+            arrayOfNumber1.push(i);
+        };
+
+    };
+
+    for (let i = 1; i <= randomNumber2; i += 1) {
+        if (randomNumber2 % i === 0) {
+            arrayOfNumber2.push(i);
+        };
+
+    };
+
+    let equalNumbers = [];
+
+    for (const element of arrayOfNumber1) {
+        if (arrayOfNumber2.includes(element)) {
+            equalNumbers.push(element);
+        };
+
+    };
+
+    const rightAnswer = Math.max(...equalNumbers);
+    
+    return [randomTwoNumbers, rightAnswer];
+}
+
+export default startBrainGcdGame;
