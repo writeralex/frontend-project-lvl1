d918f5c202f994eb565546831932d82164ec4a2f
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-06-29 20:22:35 +0300

    refactoring prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,14 +6,15 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (randomNumber) => {
   for (let i = 2; i < randomNumber; i += 1) {
     if (randomNumber !== i && randomNumber % i === 0) {
-      return 'no';
+      return false;
     }
-  } return 'yes';
+  } return true;
 };
 
 export const brainPrime = () => {
   const randomNumber = randomNumberGenerator(2, 10);
-  const rightAnswer = isPrime(randomNumber);
+  const rightAnswer = (isPrime(randomNumber) ? 'yes' : 'no');
+
   return [randomNumber, rightAnswer];
 };
 
